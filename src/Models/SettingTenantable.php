63bd44d1b82b13b570acf35cb9c5108b67a6bebf
Abdelrahman Omran <me@omranic.com>
2023-07-31 12:27:31 +0100

    Simplify imports by removing unnecessary use statements

--- src/Models/SettingTenantable.php
+++ src/Models/SettingTenantable.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 namespace Cortex\Settings\Models;
 
 use Rinvex\Tenants\Traits\Tenantable;
-use Cortex\Settings\Models\Setting as BaseSetting;
 
-class SettingTenantable extends BaseSetting
+class SettingTenantable extends Setting
 {
     use Tenantable;
 }
