6731fceb7f62f4ab6a1f2689210809d5700725a0
Abdelrahman Omran <me@omranic.com>
2023-08-16 16:09:18 +0100

    Add @TODO note for future update

--- src/Models/SettingTenantable.php
+++ src/Models/SettingTenantable.php
@@ -9,4 +9,6 @@ use Rinvex\Tenants\Traits\Tenantable;
 class SettingTenantable extends Setting
 {
     use Tenantable;
+    // @TODO: We need to override validation rules to change the unqiue key to allow unique per tenant instead of per database!
+    // 'key' => 'required|max:150|unique:'.config('rinvex.settings.models.setting').',key',
 }
